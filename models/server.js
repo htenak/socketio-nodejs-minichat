b7ee20a9360abf99936a1ecc82f815d554cb009b
kaneth <kaneth@kaneth.com>
2023-09-01 18:20:39 -0500

    primer commit

---
+++ models/server.js
@@ -0,0 +1,44 @@
+// servidor express
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const path = require("path");
+
+const Sockets = require("./sockets");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+
+    // Http server
+    this.server = http.createServer(this.app);
+
+    // configuración de sockets
+    this.io = socketio(this.server, {});
+  }
+
+  middlewares() {
+    // desplegar archivo publico
+    this.app.use(express.static(path.resolve(__dirname, "../public")));
+  }
+
+  configurarSockets() {
+    new Sockets(this.io);
+  }
+
+  execute() {
+    // inicializar middlewares
+    this.middlewares();
+
+    //inicializar sockets
+    this.configurarSockets();
+
+    // inicializar server
+    this.server.listen(this.port, () => {
+      console.log("Server corriendo en el puerto: ", this.port);
+    });
+  }
+}
+
+module.exports = Server;
